e652dead6e6d8ab7e99cc522bfe62c7d42acc553
sumitkawale <sumitkawale2002@gmail.com>
2021-08-29 00:59:01 +0530

    saving

--- topColelgeList.js
+++ topColelgeList.js
@@ -3,6 +3,8 @@ let college = [
         name: 'Government College of Engineering',
         location: 'aurangabad',
         fee: 25000,
+        intake: 9,
+        cutoff_lastYear: "SC:91 | OBC: 93.20",
         placement: 60,
         average: 3.6,
         highest: 8
@@ -11,6 +13,8 @@ let college = [
         name: 'Government College of Engineering',
         location: 'karad',
         fee: 65000,
+        intake: 10,
+        cutoff_lastYear: "SC:93.43 | OBC:93.03",
         placement: 85,
         average: 4.5,
         highest: 12
@@ -19,6 +23,8 @@ let college = [
         name: 'VJTI = Veermata Jijabai Technological Institute',
         location: 'mumbai',
         fee: 82000,
+        intake: 8,
+        cutoff_lastYear: "SC:95.33 | OBC:97.31",
         placement: 74.04,
         average: 7.65,
         highest: 36
@@ -27,6 +33,8 @@ let college = [
         name: 'VIT = Vishvakarma Institute of Technology',
         location: 'pune',
         fee: 170000,
+        intake: 31,
+        cutoff_lastYear: "SC:92.97 | OBC:94.91",
         placement: 72,
         average: 5.5,
         highest: 28
@@ -35,6 +43,7 @@ let college = [
         name: 'VNIT = Vishvesvarya National Institute of Technology',
         location: 'nagpur',
         fee: 100000,
+        intake: -1,
         placement: 70,
         average: 6.5,
         highest: 22
@@ -42,15 +51,18 @@ let college = [
     {
         name: 'Government College of Engineering',
         location: 'amravati',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: 10,
+        cutoff_lastYear: "SC: 90.13 | OBC: 92.86",
+        placement: -1,
+        average: 4.3,
+        highest: 17
     },
     {
         name: 'SPCE = Sardar Patel College of Engineering',
         location: 'mumbai',
         fee: 88000,
+        intake: 12,
         placement: 75,
         average: 5,
         highest: 21
@@ -59,6 +71,8 @@ let college = [
         name: 'Shree Guru Gobind Singhji Institute of Engineering and Technology',
         location: 'nanded',
         fee: 60000,
+        intake: 15,
+        cutoff_lastYear: "SC: 90.97 | OBC: 93.20",
         placement: 55,
         average: 5,
         highest: 12
@@ -66,14 +80,17 @@ let college = [
     {
         name: 'Yashwantrao Chavhan College of Engineering',
         location: 'nagpur',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     }, {
         name: 'WCE = Walchand College Of Engineering',
         location: 'sangli',
         fee: 85000,
+        intake: 12,
+        cutoff_lastYear: "SC: 92.75 | OBC: 95.19",
         placement: 70,
         average: 5,
         highest: 21
@@ -81,15 +98,18 @@ let college = [
     {
         name: 'Shree Ramdevbaba College of Engineering',
         location: 'nagpur',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'COEP = College Of Engineering Pune',
         location: 'pune',
         fee: 65000,
+        intake: 16,
+        cutoff_lastYear: "SC:95.09 | OBC:97.03",
         placement: 79.53,
         average: 6.5,
         highest: 39
@@ -99,6 +119,8 @@ let college = [
         name: 'PICT = Pune Institute Of Computer Technology',
         location: 'pune',
         fee: 95000,
+        intake: 27,
+        cutoff_lastYear: "SC:93.83 | OBC:96.00",
         placement: 85,
         average: 5,
         highest: 16.5
@@ -107,6 +129,8 @@ let college = [
         name: 'PCCOE = Pimpri Chinchwad College of Engineering',
         location: 'pune',
         fee: 140000,
+        intake: 26,
+        cutoff_lastYear: "SC: 91.69 | OBC: 93.94",
         placement: 55,
         average: 6.5,
         highest: 11
@@ -115,6 +139,7 @@ let college = [
         name: 'MIT-WPU = MIT World Peace University',
         location: 'pune',
         fee: 310000,
+        intake: -1,
         placement: 80,
         average: 3,
         highest: 37
@@ -122,25 +147,30 @@ let college = [
     {
         name: 'Sinhagad College of Pune',
         location: 'pune',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: 24,
+        cutoff_lastYear: "SC: 88 |OBC: 91.42",
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'Modern College of Engineering Pune',
         location: 'pune',
-        fee: 99000,
+        fee: 5739,
+        'fee for OBC': 60543,
         intake: 12,
-        placement: 0,
+        cutoff_lastYear: "SC: 88.92 |OBC: 90.63",
+        placement: -1,
         average: 4,
-        highest: 0
+        highest: -1
     },
     {
         name: 'Dr. D.Y.Patil Institute of Technology',
         location: 'pune',
-        fee: 5739,
-        'fee for OBC': 60543,
+        fee: 130000,
+        intake: 17,
+        cutoff_lastYear: "SC:88.97 | OBC:92.57",
         placement: 50,
         average: 4.5,
         highest: 30
@@ -149,14 +179,18 @@ let college = [
         name: 'All India Shri Shivaji Memorial Society College of Engineering',
         location: 'pune',
         fee: 140000,
+        intake: 9,
+        cutoff_lastYear: "SC: 88.40 | OBC: 91.77",
         placement: 50,
         average: 4,
         highest: 12
     },
     {
-        name: 'Government College of Engineering and Research Avasari',
+        name: 'Government College of Engineering and Research Avsari',
         location: 'pune',
         fee: 28000,
+        intake: 11,
+        cutoff_lastYear: "SC: 90.17 | OBC: 92.63",
         placement: 60,
         average: 3.5,
         highest: 91
@@ -164,15 +198,18 @@ let college = [
     {
         name: 'ICT = Institute of Chemical Technology',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'Sardar Patel Institute of Technology',
         location: 'mumbai',
         fee: 11550,
+        intake: 7,
+        cutoff_lastYear: "SC:96.69 | OBC:95.03",
         placement: 100,
         average: 11,
         highest: 42
@@ -180,50 +217,56 @@ let college = [
     {
         name: 'DJ Sanghli = Dwarkadas Jeevandas Sanghli',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'Somya college',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: 6,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'VESIT = Vivekanand Education Society Institute of Technology',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'TSEC = Thadomal Shahani Engineering College',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: 'RAIT = Ramrao Adik Institute of Technology',
         location: 'mumbai',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
     {
         name: '',
         location: '',
-        fee: 0,
-        placement: 0,
-        average: 0,
-        highest: 0
+        fee: -1,
+        intake: -1,
+        placement: -1,
+        average: -1,
+        highest: -1
     },
 
 ];
