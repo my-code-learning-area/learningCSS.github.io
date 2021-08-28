d39edf408798de69c5e3c285128ce701c39be31f
sumitkawale <sumitkawale2002@gmail.com>
2021-08-28 16:57:58 +0530

    saving

---
+++ colleges.js
@@ -0,0 +1,37 @@
+function change(key) {
+    college = college.slice(0);
+    college.sort(function (a, b) {
+        if (key == 'fee') {
+            return a[key] - b[key];
+
+        } else {
+            return b[key] - a[key];
+        }
+    });
+
+    // creating output html
+    let html = "<div>"
+    for (let i = 0; i < college.length; i++) {
+        html += "<hr>"
+        html += "<ul>"
+        html += i + 1
+        let c = college[i];
+        for (let key in c) {
+            html += "<li>"
+            html += "<b>"
+            html += key
+            html += ": </b>"
+            html += c[key]
+            html += "</li>"
+        }
+        html += "</ul>"
+        if (((i + 1) % 3 === 0)) {
+            console.log("yes")
+            html += '<div class="pagebreak"></div>'
+        }
+    }
+    // adding html
+    html += "</div>"
+    document.getElementById("output").innerHTML = html;
+}
+change('placement');
