8da77e8ae7f775a07b845f0848739ed7ea3b2326
sumitkawale <sumitkawale2002@gmail.com>
2021-09-02 21:03:22 +0530

    saving update

--- colleges.js
+++ colleges.js
@@ -27,7 +27,7 @@ function change(key) {
             // }
         }
         html += "</ul>"
-        if (((i + 1) % 3 === 0)) {
+        if (((i + 1) % 2 === 0)) {
             html += '<div class="pagebreak"></div>'
         }
     }
